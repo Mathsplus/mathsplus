f34d9579783b18ff0812e122a892e1d5f9b1bf77
Mathsplus <98729845+Mathsplus@users.noreply.github.com>
2022-01-31 11:49:27 +0530

    Add files via upload

---
+++ VIDEO PLAY/video-list.js
@@ -0,0 +1,63 @@
+let allVideos = [
+   {
+      name: "Computer Monitor Shape",
+      src: "./media/1",
+      id: "vid_1"
+   },
+   {
+      name: "Button Hover Effect",
+      src: "./media/2",
+      id: "vid_2"
+   },
+   {
+      name: "Computer Monitor Shape",
+      src: "./media/1",
+      id: "vid_1"
+   },
+   {
+      name: "Button Hover Effect",
+      src: "./media/2",
+      id: "vid_2"
+   },
+   {
+      name: "Computer Monitor Shape",
+      src: "./media/1",
+      id: "vid_1"
+   },
+   {
+      name: "Button Hover Effect",
+      src: "./media/2",
+      id: "vid_2"
+   },
+   {
+      name: "Computer Monitor Shape",
+      src: "./media/1",
+      id: "vid_1"
+   },
+   {
+      name: "Button Hover Effect",
+      src: "./media/2",
+      id: "vid_2"
+   },
+   {
+      name: "Computer Monitor Shape",
+      src: "./media/1",
+      id: "vid_1"
+   },
+   {
+      name: "Button Hover Effect",
+      src: "./media/2",
+      id: "vid_2"
+   },
+   {
+      name: "Computer Monitor Shape",
+      src: "./media/1",
+      id: "vid_1"
+   },
+   {
+      name: "Button Hover Effect",
+      src: "./media/2",
+      id: "vid_2"
+   },
+
+]
