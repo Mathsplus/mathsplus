91fbea11915e228943bd1ddb19cf1e156032088a
Mathsplus <98729845+Mathsplus@users.noreply.github.com>
2022-02-02 11:12:34 +0530

    Update login2.js

--- login2.js
+++ login2.js
@@ -5,7 +5,7 @@ var password=document.getElementById("password").value;
 if(username=="admin"&& password=="user")
 {
     
-	window.open("menu.html","_self");
+	window.open("menu.html","_parent");
     return false;
 
 }
